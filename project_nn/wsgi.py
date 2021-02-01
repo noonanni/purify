2b6b56be249d5ed280c6c608249ef1400321e7b0
Niall Noonan <noonanni@tcd.ie>
2021-01-31 21:12:44 -0500

    Update wsgi.py

--- project_nn/wsgi.py
+++ project_nn/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+sys.path.append('/opt/bitnami/projects/project_nn')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/project_nn/egg_cache")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "project_nn.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_nn.settings')
-
 application = get_wsgi_application()
+
+
